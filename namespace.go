fa2c3f05650cd92056ecd1f7f7a0959b393276c4
yehan2002 <yehanjaya2002@gmail.com>
2021-02-06 17:34:14 +0530

    Update namespace code.

--- namespace.go
+++ namespace.go
@@ -4,12 +4,11 @@ import (
 	"fmt"
 	"strings"
 	"sync"
-	"unicode"
 )
 
 //DefaultNamespace this is the namespace used for minecraft items/effects ect.
 //Plugins should use their own namespace if they aren't overriding vanila features
-var DefaultNamespace Namespace = func() Namespace { v, _ := GetNamespace("minecraft"); return v }()
+var DefaultNamespace Namespace = MustGetNamespace("minecraft")
 
 var namespaceMap, namespacedKeyMap sync.Map
 
@@ -19,6 +18,7 @@ type Namespace interface {
 	String() string
 
 	Key(s string) (NamespacedKey, error)
+	MustKey(s string) NamespacedKey
 
 	namespace() *namespace
 }
@@ -34,29 +34,6 @@ type NamespacedKey interface {
 	namespacedKey() *namespacedKey
 }
 
-type namespace struct {
-	name string
-}
-
-func (n *namespace) String() string { return n.name }
-
-func (n *namespace) Key(s string) (NamespacedKey, error) { return GetNamespacedKey(n, s) }
-
-func (n *namespace) namespace() *namespace { return n }
-
-type namespacedKey struct {
-	namespace *namespace
-	key       string
-}
-
-func (n *namespacedKey) String() string { return n.namespace.name + ":" + n.key }
-
-func (n *namespacedKey) Namespace() Namespace { return n.namespace }
-
-func (n *namespacedKey) Key() string { return n.key }
-
-func (n *namespacedKey) namespacedKey() *namespacedKey { return n }
-
 //GetNamespace gets the namespace or creates a new namespace if it does not exist.
 func GetNamespace(s string) (Namespace, error) {
 	s = strings.ToLower(s)
@@ -68,22 +45,9 @@ func GetNamespace(s string) (Namespace, error) {
 	return r.(*namespace), nil
 }
 
-//GetNamespacedKey get the namespaced key
-func GetNamespacedKey(n Namespace, k string) (NamespacedKey, error) {
-	k = strings.ToLower(k)
-	if n == nil {
-		return nil, fmt.Errorf("The provided namespace is nil")
-	}
-	if len(strings.TrimSpace(k)) == 0 || !isValidNamespacedKey(k) {
-		return nil, fmt.Errorf("Invalid namespaced key: '" + k + "'")
-	}
-
-	r, _ := namespacedKeyMap.LoadOrStore(k, &namespacedKey{namespace: n.namespace(), key: k})
-	return r.(*namespacedKey), nil
-}
-
-//NamespacedKeyFor get the given string
-func NamespacedKeyFor(s string) (NamespacedKey, error) {
+//GetNamespacedKey get the given string
+func GetNamespacedKey(s string) (NamespacedKey, error) {
+	s = strings.ToLower(strings.TrimSpace(s))
 	v := strings.Split(s, ":")
 	if len(v) != 2 {
 		return nil, fmt.Errorf("namespace: NamespacedKeyFor:Invalid string provided")
@@ -92,27 +56,27 @@ func NamespacedKeyFor(s string) (NamespacedKey, error) {
 	if err != nil {
 		return nil, err
 	}
-	key, err := GetNamespacedKey(namespace, v[1])
+
+	key, err := namespace.Key(v[1])
 
 	return key, err
 }
 
-//See https://www.minecraft.net/en-us/article/minecraft-snapshot-17w43a
-
-func isValidNamespace(s string) bool {
-	for _, b := range []rune(s) {
-		if !(unicode.IsLower(b) || unicode.IsDigit(b) || b == '-' || b == '_') {
-			return false
-		}
+//MustGetNamespace gets or creates the given namespace. This panics if the given namespace is not valid
+func MustGetNamespace(s string) Namespace {
+	ns, err := GetNamespace(s)
+	if err != nil {
+		panic(err)
 	}
-	return true
+	return ns
 }
 
-func isValidNamespacedKey(s string) bool {
-	for _, b := range []rune(s) {
-		if !(unicode.IsLower(b) || unicode.IsDigit(b) || b == '-' || b == '_' || b == '/' || b == '.') {
-			return false
-		}
+//MustGetNamespacedKey gets the namespaced key for the give string.
+//panics if the given key is invalid.
+func MustGetNamespacedKey(s string) NamespacedKey {
+	key, err := GetNamespacedKey(s)
+	if err != nil {
+		panic(err)
 	}
-	return true
+	return key
 }
